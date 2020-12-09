750f90f7b491b4ffc209d0f972f69b4d2fa0bb0b
David <dgibbs8089@gmail.com>
2020-12-08 18:16:40 -0600

    added code to the other folders

---
+++ src/FreedomApplication.java
@@ -0,0 +1,16 @@
+package com.freedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main class 
+ */
+
+@SpringBootApplication
+public class FreedomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FreedomApplication.class, args);
+  }
+
+}
